f36c5b2677b0d91c067f60352fa78160eb251502
vladukha <vladic.p@gmail.com>
2022-11-08 00:22:01 +0300

    Initial Commit

---
+++ mvvm-kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm-kit
+//
+//  Created by Владислав Пермяков on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
